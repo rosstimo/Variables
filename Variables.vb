39dd411c6ebedd71b6d0790730f4fb8bf68e4477
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2020-08-21 08:36:24 -0600

    Add project files.

---
+++ Variables.vb
@@ -0,0 +1,38 @@
+﻿Option Strict On
+Option Explicit On
+
+
+Module Variables
+
+    Sub Main()
+        Dim myFirstNumber As Integer
+        Dim mySecondNumber As Integer
+        Dim aLittleString As String
+        Dim someSortOfTest As Boolean
+
+
+        myFirstNumber = 42
+
+        Console.WriteLine(myFirstNumber)
+        Console.ReadLine()
+
+        aLittleString = "some text"
+
+        Console.WriteLine(aLittleString)
+        Console.ReadLine()
+
+        'someSortOfTest = True
+
+        Console.WriteLine(someSortOfTest)
+        Console.ReadLine()
+
+        'break it with wrong data type
+        'mySecondNumber = "5"
+
+        'x = 7
+        'Console.WriteLine(x)
+        'Console.ReadLine()
+
+    End Sub
+
+End Module
